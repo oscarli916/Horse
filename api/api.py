259d4320733c9779c08f7739c8a3086160a313b9
Oscar Li <oscar_li916@yahoo.com.hk>
2021-06-12 17:17:01 +0800

    Added all.json

--- api/api.py
+++ api/api.py
@@ -15,18 +15,28 @@ class HorseOddsAPI(API):
     ### raceno: str or empty
     def get_horse_odds_json_file_path(self, race: str = "") -> str:
         if race == "":
-            return os.path.join(self.api_horse_odds_folder, "all")
+            return os.path.join(self.api_horse_odds_folder, "all.json")
         return os.path.join(self.api_horse_odds_folder, race + ".json")
 
     def update_horse_odds_api(self, win_place_odds: dict) -> None:
-        ### Update each race
+        self.update_each_horse_odds_api(win_place_odds)
+        
+        self.update_all_horse_odds_api(win_place_odds)
+
+    ### Update each race
+    def update_each_horse_odds_api(self, win_place_odds: dict) -> None:
         for race_odds in win_place_odds:
-            print(f"[INFO]: Updating {race_odds} json file")
+            print(f"[INFO]: Updating {race_odds}.json file")
             horse_odds_file = self.get_horse_odds_json_file_path(race_odds)
             with open(horse_odds_file, "w") as f:
                 json.dump(win_place_odds[race_odds], f)
-        ### Update all race
     
+    ### Update all race
+    def update_all_horse_odds_api(self, win_place_odds: dict) -> None:
+        print(f"[INFO]: Updating all.json file")
+        horse_odds_file = self.get_horse_odds_json_file_path()
+        with open(horse_odds_file, "w") as f:
+            json.dump(win_place_odds, f)
 
 if __name__ == "__main__":
     # api = API()
