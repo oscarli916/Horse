278b605bc6b4bd347c7555e98480b79cfb499ca4
Oscar Li <oscar_li916@yahoo.com.hk>
2021-06-12 17:09:47 +0800

    Added api route

--- web/app.py
+++ web/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from api.api import HorseOddsAPI
+import json
 
 def make_app(scraper):
     app = Flask(__name__)
+    api = HorseOddsAPI()
 
+    ### Website
     @app.route("/")
     def home():
         return render_template("index.html")
@@ -15,4 +19,16 @@ def make_app(scraper):
     def horse_odds_race(race):
         return render_template(f"horse_odds_{race}.html", race=race)
     
+    ### API
+    @app.route("/api/horse_odds", defaults={"race":""})
+    @app.route("/api/horse_odds/<string:race>")
+    def api_horse_odds(race):
+        if race == "":
+            horse_odds_file = api.get_horse_odds_json_file_path()
+        else:
+            horse_odds_file = api.get_horse_odds_json_file_path(race)
+        with open(horse_odds_file, "r") as f:
+            data = json.load(f)
+        return jsonify(data)
+
     return app
